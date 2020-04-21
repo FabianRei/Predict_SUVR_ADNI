6c69c27a30cbbc1f137520670b988569f0051391
Fabianrei <gFriessm20*>
2020-04-21 15:58:42 +0200

    more analysis

--- dl/neural_network/multi_task_loss.py
+++ dl/neural_network/multi_task_loss.py
@@ -26,7 +26,7 @@ class CustomMultiWrapper:
         curr_loss_apoe = curr_loss_apoe/20
         comb_loss = torch.stack([curr_loss_suvr, curr_loss_age, curr_loss_apoe])
         # multi_task_loss = self.multi_loss(comb_loss)
-        print((curr_loss_suvr + curr_loss_age + curr_loss_apoe))
+        print(comb_loss)
         return (curr_loss_suvr + curr_loss_age + curr_loss_apoe)/3
 
     def to_train(self):
